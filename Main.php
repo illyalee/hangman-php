826ff4418ee81d11ddaac34423c4457979962089
illyalee <ilia.lee.573@gmail.com>
2023-11-18 11:12:27 +0200

    add convert word functionality

--- Main.php
+++ Main.php
@@ -3,14 +3,15 @@
 class Main
 {
     public $word;
-
+    public $roundWord;
+    public $mistakeCounter = 0;
     function startGame() {
         $userInput = (string)readline("Wanna play?: (y/n)");
         $answer = $this->checkStartGameAnswer($userInput);
         if($answer === true) {
-            print 'game started!';
-            $this->getRandomWord();
-
+            print "game started!\n";
+            $this->word = $this->getRandomWord();
+            $this->startRound();
         } else {
             print 'exit';
             die;
@@ -29,11 +30,31 @@ class Main
         $minNum = 0;
         $maxNum = sizeof($words);
         $randomNumber = rand($minNum, $maxNum);
-        $word = $words[$randomNumber];
-        return $word;
+        return $words[$randomNumber];
     }
 
     function getFile($name) {
         return $words = file($name,0, null);
     }
+    function normalizeChar($str, $index) {
+        $char = mb_substr($str, $index, 1, 'UTF-8');
+        return $char;
+    }
+
+    function convertWord($str, $letter = '') {
+        if(empty($letter)) {
+            $this->roundWord = '';
+            for ($i = 0; $i <=  mb_strlen($str) - 1; $i++) {
+                $char = $this->normalizeChar($str, $i);
+                $this->roundWord .= '*';
+            }
+
+        }
+    }
+    function startRound() {
+    if(empty($this->roundWord)) {
+        $this->convertWord($this->word);
+    }
+}
+//    function
 }
